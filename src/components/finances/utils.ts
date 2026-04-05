f50b94164e2e948b225a85ff059d7673934160f3
Eliam Paredes <67697491+MaravStress@users.noreply.github.com>
2026-04-05 10:21:03 -0400

    Limpiamos finanzas

--- src/components/finances/utils.ts
+++
@@ -1,9 +0,0 @@
-import type { Currency } from '../../types/finances';
-
-export const convertToUSD = (amount: number, currency: Currency, exchangeRate: number) => {
-    return currency === 'USD' ? amount : amount / exchangeRate;
-};
-
-export const formatCurrency = (amount: number) => {
-    return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(amount);
-};
