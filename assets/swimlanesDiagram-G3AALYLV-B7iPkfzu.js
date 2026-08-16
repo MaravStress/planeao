da6f38d29e0570ac0db28cd445831c6e6b6c7c84
Eliam Paredes <eliamjesus@gmail.com>
2026-08-16 16:23:00 -0400

    Updates

---
+++ assets/swimlanesDiagram-G3AALYLV-B7iPkfzu.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U-BFmCedKs.js";import{_ as a}from"./index-CQ5S-jkA.js";import"./chunk-5VM5RSS4-wK77c1HB.js";import"./chunk-XXDRQBXY-Dbl4cwQV.js";import"./chunk-VR4S4FIN-DTP8qApn.js";import"./chunk-32BRIVSS-DTNlJin1.js";import"./channel-CeYyEiaz.js";import"./index-Caiwmsx-.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
--- assets/swimlanesDiagram-G3AALYLV-Ci5o-vuU.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U-D-Ku_xwj.js";import{_ as a}from"./index-5RDYhH7h.js";import"./chunk-5VM5RSS4-Cb7LYtZq.js";import"./chunk-XXDRQBXY-CbykkxLM.js";import"./chunk-VR4S4FIN-M43lGgnP.js";import"./chunk-32BRIVSS-CdDzb9Fw.js";import"./channel-DT1-5Zn0.js";import"./index-DyLVWaO8.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
