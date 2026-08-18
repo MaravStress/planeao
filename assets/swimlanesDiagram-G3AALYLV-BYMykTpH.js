7379214161a65fdac3cc3b04d7ffc7c7f0cd7583
Eliam Paredes <eliamjesus@gmail.com>
2026-08-17 21:25:11 -0400

    Updates

---
+++ assets/swimlanesDiagram-G3AALYLV-BYMykTpH.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U-BsnQx4x0.js";import{_ as a}from"./index-D51ZyjoK.js";import"./chunk-5VM5RSS4-dDTFhUw1.js";import"./chunk-XXDRQBXY-FMqlxCMZ.js";import"./chunk-VR4S4FIN-E9JpumLH.js";import"./chunk-32BRIVSS-sVWaCNQb.js";import"./channel-C6XA4n05.js";import"./index-Ijve_-ij.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
