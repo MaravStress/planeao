99f8544aa891d1eb636ce18f4f92b1d8df5553fa
Eliam Paredes <eliamjesus@gmail.com>
2026-09-05 16:44:42 -0400

    Updates

--- assets/channel-DYITrp9g.js
+++
@@ -1,1 +0,0 @@
-import{U as a,C as n}from"./index-Wha_3s6O.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
--- assets/swimlanesDiagram-G3AALYLV-B0uUS_Yg.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U-CmFd63us.js";import{_ as a}from"./index-Wha_3s6O.js";import"./chunk-5VM5RSS4-CZlYeRya.js";import"./chunk-XXDRQBXY-D0Xrado8.js";import"./chunk-VR4S4FIN-Dx4MbPdU.js";import"./chunk-32BRIVSS-B2ygv8HB.js";import"./channel-DYITrp9g.js";import"./index-Zg03V2tC.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
